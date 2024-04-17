ace115e639902722a4a1a7cf3b339dd6bfb4aef7
Mark dechamps <mark.dechamps@mateco.eu>
2024-04-17 10:19:36 +0200

    list topcis

--- src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
+++ src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
@@ -1,5 +1,7 @@
 package be.ccaak.kafkatoy.commands;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
@@ -15,6 +17,19 @@ import java.util.concurrent.ExecutionException;
 @Slf4j
 public class ShellCommands {
 
+    private AdminClient admin;
+
+    @PostConstruct
+    public void init() {
+        log.info("Initializing adminClient");
+        admin = AdminClient.create(Map.of(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"));
+    }
+    @PreDestroy
+    public void shutdown() {
+        log.info("Closing adminClient");
+        admin.close();
+    }
+
     @ShellMethod
     public void publish(String msg, @ShellOption(valueProvider = TopicValueProvider.class) String topic) {
         System.out.println("Should publish :" + msg + " on topic " + topic);
@@ -23,7 +38,6 @@ public class ShellCommands {
 
     @ShellMethod
     public void topics() throws ExecutionException, InterruptedException {
-        var admin = AdminClient.create(Map.of(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"));
         var topics = admin.listTopics();
         topics.names().get()
                 .stream().filter(s->!s.startsWith("_"))
