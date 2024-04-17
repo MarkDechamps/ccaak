5ac170530a6a218a52187998052710f3b4d1e815
Mark dechamps <mark.dechamps@mateco.eu>
2024-04-17 10:25:18 +0200

    create topics

--- src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
+++ src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
@@ -5,12 +5,15 @@ import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 @ShellComponent
@@ -43,4 +46,12 @@ public class ShellCommands {
                 .stream().filter(s->!s.startsWith("_"))
                 .forEach(System.out::println);
     }
+    @ShellMethod
+    public void createTopic(String name) throws ExecutionException, InterruptedException {
+        NewTopic newTopic = new NewTopic(name, Optional.empty(),Optional.empty());
+        var createTopicsResult = admin.createTopics(List.of(newTopic));
+        int partitions = createTopicsResult.numPartitions(name).get();
+        log.info("Topic created {} with partitions {}!", name, partitions);
+    }
+
 }
