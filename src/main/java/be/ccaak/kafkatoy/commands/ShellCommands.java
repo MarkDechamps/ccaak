66c4f9d8e2e789f17cbb07c815ffeaa97f8094ec
Mark dechamps <mark.dechamps@mateco.eu>
2024-04-17 10:16:37 +0200

    list topcis

--- src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
+++ src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
@@ -1,16 +1,32 @@
 package be.ccaak.kafkatoy.commands;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 @ShellComponent
+@Slf4j
 public class ShellCommands {
 
-    @ShellMethod(value = "publish", key = "publish")
+    @ShellMethod
     public void publish(String msg, @ShellOption(valueProvider = TopicValueProvider.class) String topic) {
-        System.out.println("Should publish :"+msg+" on topic "+topic);
+        System.out.println("Should publish :" + msg + " on topic " + topic);
         KafkaProducer producer;
+    }
+
+    @ShellMethod
+    public void topics() throws ExecutionException, InterruptedException {
+        var admin = AdminClient.create(Map.of(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"));
+        var topics = admin.listTopics();
+        topics.names().get()
+                .stream().filter(s->!s.startsWith("_"))
+                .forEach(System.out::println);
     }
 }
