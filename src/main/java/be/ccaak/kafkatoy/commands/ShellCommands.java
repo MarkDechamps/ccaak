b707a152a98437f6a3351e81f07639c804013ce9
Mark dechamps <mark.dechamps@mateco.eu>
2024-04-17 13:58:35 +0200

    add amount of messages and consumer

--- src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
+++ src/main/java/be/ccaak/kafkatoy/commands/ShellCommands.java
@@ -1,22 +1,27 @@
 package be.ccaak.kafkatoy.commands;
 
-import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
-
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
+import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
 
 @ShellComponent
 @Slf4j
@@ -37,13 +42,33 @@ public class ShellCommands {
     }
 
     @ShellMethod
-    public void publish(String msg, @ShellOption(valueProvider = TopicValueProvider.class) String topic) throws ExecutionException, InterruptedException {
+    public void publish(String msg, @ShellOption(valueProvider = TopicValueProvider.class) String topic, @ShellOption(defaultValue = "1") Integer amount) {
         System.out.println("Should publish :" + msg + " on topic " + topic);
-        var producer = new KafkaProducer<String, String>(Map.of("bootstrap.servers", "localhost:9092",
+        try (var producer = new KafkaProducer<String, String>(producerConfig())) {
+            IntStream.rangeClosed(0, amount).forEach(i -> {
+                try {
+                    var result = producer.send(new ProducerRecord<>(topic, msg)).get();
+                    log.info("Message published: {} ", result);
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+
+            });
+        }
+    }
+
+    private static Map<String, Object> producerConfig() {
+        return Map.of("bootstrap.servers", "localhost:9092",
                 "key.serializer", "org.apache.kafka.common.serialization.StringSerializer",
-                "value.serializer", "org.apache.kafka.common.serialization.StringSerializer"));
-        var result = producer.send(new ProducerRecord<>(topic, msg)).get();
-        log.info("Message published: {} ", result);
+                "value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    }
+
+    private static Map<String, Object> consumerConfig(String groupId) {
+        return Map.of("bootstrap.servers", "localhost:9092",
+                "key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer",
+                "value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer",
+                //    "auto.offset.reset","earliest",
+                "group.id", groupId);
     }
 
     @ShellMethod
@@ -60,6 +85,23 @@ public class ShellCommands {
         var createTopicsResult = admin.createTopics(List.of(newTopic));
         int partitions = createTopicsResult.numPartitions(name).get();
         log.info("Topic created {} with partitions {}!", name, partitions);
+    }
+
+    @ShellMethod
+    public void consume(String topic, @ShellOption(defaultValue = "my-group") String groupId) {
+        try (var consumer = new KafkaConsumer<String, String>(consumerConfig(groupId))) {
+            consumer.subscribe(List.of(topic));
+            int count = 0;
+            while (count < 3) {
+                var result = consumer.poll(Duration.of(10, ChronoUnit.SECONDS));
+                log.info("Read:{}", result.count());
+
+                result.forEach((ConsumerRecord<String, String> record) -> {
+                    log.info("=>{}", record.toString());
+                });
+                count++;
+            }
+        }
     }
 
 }
